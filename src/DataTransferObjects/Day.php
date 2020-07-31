7737b4d8abd3f87d314078bc71c798944615ef3a
Chris Rhymes <christopher.rhymes@networkrail.co.uk>
2020-07-31 14:16:16 +0100

    Add monthly calendar

---
+++ src/DataTransferObjects/Day.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace NRBusinessSystems\LaraMoodle\DataTransferObjects;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+class Day extends DataTransferObject
+{
+    /** @var integer */
+    public $seconds;
+
+    /** @var integer */
+    public $minutes;
+
+    /** @var integer */
+    public $hours;
+
+    /** @var integer */
+    public $mday;
+
+    /** @var integer */
+    public $wday;
+
+    /** @var integer */
+    public $year;
+
+    /** @var integer */
+    public $yday;
+
+    /** @var boolean */
+    public $istoday;
+
+    /** @var boolean */
+    public $isweekend;
+
+    /** @var integer */
+    public $timestamp;
+
+    /** @var integer */
+    public $neweventtimestamp;
+
+    /** @var string */
+    public $viewdaylink;
+
+    /** @var null|string */
+    public $viewdaylinktitle;
+
+    /** @var null|\NRBusinessSystems\LaraMoodle\DataTransferObjects\Event[] */
+    public $events;
+
+    /** @var boolean */
+    public $hasevents;
+
+    /** @var array */
+    public $calendareventtypes;
+
+    /** @var integer */
+    public $previousperiod;
+
+    /** @var integer */
+    public $nextperiod;
+
+    /** @var string */
+    public $navigation;
+
+    /** @var boolean */
+    public $haslastdayofevent;
+
+    /** @var string */
+    public $popovertitle;
+}
---
+++ src/DataTransferObjects/Event.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace NRBusinessSystems\LaraMoodle\DataTransferObjects;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+class Event extends DataTransferObject
+{
+    /** @var integer **/
+    public $id;
+
+    /** @var string **/
+    public $name;
+
+    /** @var string **/
+    public $description;
+
+    /** @var integer **/
+    public $descriptionformat;
+
+    /** @var string **/
+    public $location;
+
+    /** @var null|integer **/
+    public $categoryid;
+
+    /** @var null|integer **/
+    public $groupid;
+
+    /** @var integer **/
+    public $userid;
+
+    /** @var null|integer **/
+    public $repeatid;
+
+    /** @var null|integer **/
+    public $eventcount;
+
+    /** @var string **/
+    public $component;
+
+    /** @var string **/
+    public $modulename;
+
+    /** @var integer **/
+    public $instance;
+
+    /** @var string **/
+    public $eventtype;
+
+    /** @var integer **/
+    public $timestart;
+
+    /** @var integer **/
+    public $timeduration;
+
+    /** @var integer **/
+    public $timesort;
+
+    /** @var integer **/
+    public $visible;
+
+    /** @var integer **/
+    public $timemodified;
+
+    /** @var array **/
+    public $icon;
+
+    /** @var null|array **/
+    public $course;
+
+    /** @var array **/
+    public $subscription;
+
+    /** @var boolean **/
+    public $canedit;
+
+    /** @var boolean **/
+    public $candelete;
+
+    /** @var string **/
+    public $deleteurl;
+
+    /** @var string **/
+    public $editurl;
+
+    /** @var string **/
+    public $viewurl;
+
+    /** @var string **/
+    public $formattedtime;
+
+    /** @var boolean **/
+    public $isactionevent;
+
+    /** @var boolean **/
+    public $iscourseevent;
+
+    /** @var boolean **/
+    public $iscategoryevent;
+
+    /** @var null|string **/
+    public $groupname;
+
+    /** @var string **/
+    public $normalisedeventtype;
+
+    /** @var string **/
+    public $normalisedeventtypetext;
+
+    /** @var string **/
+    public $url;
+
+    /** @var boolean **/
+    public $islastday;
+
+    /** @var string **/
+    public $popupname;
+
+    /** @var boolean **/
+    public $draggable;
+}
