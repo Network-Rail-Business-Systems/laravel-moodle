e1a368e1967e5803527a2c1aa528b30c77e1a1a1
Anthony Edmonds <anthony.edmonds@live.co.uk>
2025-01-23 08:52:59 +0000

    Updated provider to register middleware

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,8 +2,18 @@
 
 namespace NetworkRailBusinessSystems\LaravelMoodle;
 
-class ServiceProvider extends \Illuminate\Support\ServiceProvider
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use NetworkRailBusinessSystems\LaravelMoodle\Middleware\MoodleToken;
+
+class ServiceProvider extends BaseServiceProvider
 {
+    public function register(): void
+    {
+        parent::register();
+
+        app('router')->aliasMiddleware('laravel-moodle', MoodleToken::class);
+    }
+
     public function boot(): void
     {
         $this->publishes([
