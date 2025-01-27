161693a19fd8b020195067008e27c86fa93b9794
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2025-01-27 13:17:06 +0000

    Corrected User emulatio

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,12 +2,15 @@
 
 namespace NetworkRailBusinessSystems\LaravelMoodle;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use NetworkRailBusinessSystems\LaravelMoodle\Middleware\MoodleToken;
 
 class ServiceProvider extends BaseServiceProvider
 {
+    const string EMULATOR_URL = 'http://moodle.test';
+
     public function register(): void
     {
         parent::register();
@@ -30,24 +33,43 @@ class ServiceProvider extends BaseServiceProvider
 
     protected function startEmulator(): void
     {
+        config()->set('laravel-moodle.base_url', self::EMULATOR_URL);
+
+        /** @var class-string<Model> $modelClass */
+        $modelClass = config('laravel-moodle.user_model');
+
+        $user = $modelClass::query()->firstOrCreate([
+            'username' => 'gandalf',
+        ], [
+            'address' => '123 Fake Street',
+            'business_area' => 'Isengard',
+            'email' => 'gandalf.stormcrow@example.com',
+            'location' => 'Rohan',
+            'moodle_id' => 123,
+            'name' => 'Gandalf Stormcrow',
+            'office' => 'Rooftop',
+            'title' => 'Wizard',
+        ]);
+
         Http::fake([
-            '*/login/token.php*' => [
-                'error' => false,
+            '*login/token*' => [
                 'token' => 'abc123',
             ],
-            '*core_user_get_users&moodlewsrestformat' => [
+            '*core_user_get_users&moodlewsrestformat*' => [
                 'users' => [
-                    'moodle_id' => 123,
-                    'name' => 'Gandalf Stormcrow',
-                    'email' => 'gandalf.stormcrow@example.com',
-                    'title' => 'Wizard',
-                    'business_area' => 'Isengard',
-                    'office' => 'Rooftop',
-                    'location' => 'Rohan',
-                    'address' => '123 Fake Street',
-                    'username' => 'gandalf',
-                ]
+                    [
+                        'address' => $user->address,
+                        'city' => $user->location,
+                        'department' => $user->business_area,
+                        'description' => $user->title,
+                        'email' => $user->email,
+                        'fullname' => $user->name,
+                        'id' => $user->moodle_id,
+                        'institution' => $user->office,
+                        'username' => $user->username,
+                    ],
+                ],
             ],
-        ]);
+        ])->baseUrl(self::EMULATOR_URL);
     }
 }
